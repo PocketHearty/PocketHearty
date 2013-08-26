dd7d42ad8980fe5d06c5470f70992ccb9d07f0ae
PocketHearty <fengzee@fengzee-macbook-pro.local>
2013-08-26 20:25:36 +0800

    Add some service files & IMPORTANT NOTICE file
    List for this commit:
    
    PHGraphViewController .h/m
    PHAnalyzer .h/m
    PHLogger .h/m
    PHCommunicator .h/m
    
    IMPORTANT NOTICE
       // <----- Please read this

---
+++ PocketHearty/View Controllers/PHGraphViewController.m
@@ -0,0 +1,38 @@
+//
+//  PHGraphViewController.m
+//  PocketHearty
+//
+//  Created by 冯 子力 on 13-8-26.
+//  Copyright (c) 2013年 清华大学未来医疗兴趣团队心电手机背壳项目组. All rights reserved.
+//
+
+#import "PHGraphViewController.h"
+
+@interface PHGraphViewController ()
+
+@end
+
+@implementation PHGraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
