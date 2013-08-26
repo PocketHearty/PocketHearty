2974d3cc6f522ccc2af6a9ac13598bf02214372e
PocketHearty <fengzee@fengzee-macbook-pro.local>
2013-08-26 18:44:50 +0800

    Initial commit

---
+++ PocketHearty/PHAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  PHAppDelegate.h
+//  PocketHearty
+//
+//  Created by 冯 子力 on 13-8-26.
+//  Copyright (c) 2013年 清华大学未来医疗兴趣团队心电手机背壳项目组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PocketHeartyTests/PocketHeartyTests.m
@@ -0,0 +1,34 @@
+//
+//  PocketHeartyTests.m
+//  PocketHeartyTests
+//
+//  Created by 冯 子力 on 13-8-26.
+//  Copyright (c) 2013年 清华大学未来医疗兴趣团队心电手机背壳项目组. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PocketHeartyTests : XCTestCase
+
+@end
+
+@implementation PocketHeartyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
